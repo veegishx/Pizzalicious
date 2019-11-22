d6fcfe88a1fc7c4d1788a8d6910496407c8a7c0c
Veegish Ramdani <veegish@cyberstorm.mu>
2019-11-22 16:23:21 +0400

    Initial Commit

---
+++ src/CustomerClient/Main.java
@@ -0,0 +1,23 @@
+//package CustomerClient;
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//
+//public class Main extends Application {
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("FXMLUserInterfaces/Customer/PizzaMenu.fxml"));
+//        primaryStage.setTitle("Pizza Menu");
+//        primaryStage.setScene(new Scene(root, 1280, 700));
+//        primaryStage.show();
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
